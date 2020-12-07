925a292e529504a48a5e8283f35e578cd588bf23
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2020-12-07 15:40:42 +0000

    adding storage bucket, apigw and lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,17 +1,54 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import {Code, Function, Runtime, Tracing} from '@aws-cdk/aws-lambda';
+import {LambdaRestApi} from '@aws-cdk/aws-apigateway';
+
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    new s3.Bucket(this, 'ghoran-photo-storage', {
-      versioned: true
-    });
-    new s3.Bucket(this, 'ghoran-website-photo-upload', {
-      versioned: true,
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
-    });
-  }
+        const storageBucket = new s3.Bucket(this, 'photo-storage', {
+            versioned: true,
+            // cors: [
+            //     {
+            //         maxAge: 3000,
+            //         allowedOrigins: ["*"],
+            //         allowedHeaders: ["*"],
+            //         allowedMethods: [ s3.HttpMethods.POST,
+            //             s3.HttpMethods.PUT,
+            //             s3.HttpMethods.GET,],
+            //     },
+            // ],
+        });
+
+        const websiteBucket = new s3.Bucket(this, 'website-photo-upload', {
+            versioned: true,
+            websiteIndexDocument: "index.html",
+            publicReadAccess: true,
+        });
+
+        const getSignedUrlLambda = new Function(this, 'signed-url-lambda', {
+                runtime: Runtime.NODEJS_12_X,
+                code: Code.fromAsset('./dist/functions/add-claim'),
+                handler: 'handler.default',
+                tracing: Tracing.ACTIVE
+            }
+        );
+
+        //this construct should do all the plumbing for us - permissions to allow apigw to invoke the lambda +
+        //all traffic routed to lambda regardless of path.
+        const api = new LambdaRestApi(this, 'website-api', {
+            handler: getSignedUrlLambda
+        })
+
+        //need to allow the lambda to read/write from the website. The lambda generates the
+        // presigned URL so needs read/write permission
+
+        websiteBucket.grantReadWrite(getSignedUrlLambda)
+
+
+
+    }
 }
+
