e8c4555725292133060598b88dddebfcc100f8e6
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2020-12-21 14:59:14 +0000

    removing cognito stuff

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -95,37 +95,6 @@ export class CdkStack extends cdk.Stack {
             events: [EventType.OBJECT_CREATED],
             filters: [{prefix: 'originals/'}]
         }))
-        const userPool = new UserPool(this, "UserPool", {
-            selfSignUpEnabled: true, // Allow users to sign up
-            autoVerify: { email: true }, // Verify email addresses by sending a verification code
-            signInAliases: { email: true }, // Set email as an alias
-        });
-
-        const userPoolClient = new UserPoolClient(this, "UserPoolClient", {
-            userPool,
-            generateSecret: false, // Don't need to generate secret for web app running on browsers
-        });
-
-        const identityPool = new CfnIdentityPool(this, "IdentityPool", {
-            allowUnauthenticatedIdentities: false, // Don't allow unathenticated users
-            cognitoIdentityProviders: [
-                {
-                    clientId: userPoolClient.userPoolClientId,
-                    providerName: userPool.userPoolProviderName,
-                },
-            ],
-        });
-
-        // Export values
-        new CfnOutput(this, "UserPoolId", {
-            value: userPool.userPoolId,
-        });
-        new CfnOutput(this, "UserPoolClientId", {
-            value: userPoolClient.userPoolClientId,
-        });
-        new CfnOutput(this, "IdentityPoolId", {
-            value: identityPool.ref,
-        });
     }
 }
 
