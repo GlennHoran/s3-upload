f23b379f4f6d4a0f6f58e0251d42c163ac0d4167
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2021-01-06 14:12:54 +0000

    updating list files url

--- photo-upload-website/src/service/axiosService.js
+++ photo-upload-website/src/service/axiosService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import {formatUrl} from "../util";
 
-const apiGatewayUrl = "https://wp0r948d32.execute-api.us-east-1.amazonaws.com/prod/upload"
+const apiGatewayUrl = "https://wp0r948d32.execute-api.us-east-1.amazonaws.com/prod"
 
 //consider changing this to a post so I can send metadata (title, description etc)
-export const getPreSignedUrl = (fileName, urlTypeRequested) => axios.post(apiGatewayUrl, {
+export const getPreSignedUrl = (fileName, urlTypeRequested) => axios.post(apiGatewayUrl + '/upload', {
         fileName: fileName,
         urlTypeRequested: urlTypeRequested
 }
@@ -24,7 +24,7 @@ export const uploadFileToS3 = (url, file) => axios.put(url, file)
         return `Error: ${error.toString()}`
     });
 
-export const listObjectsFromS3 = () => axios.get(apiGatewayUrl).then(
+export const listObjectsFromS3 = () => axios.get(apiGatewayUrl+ '/s3-api').then(
     function (response) {
         console.log(`1. ${JSON.stringify(response.data.Contents)}`)
         console.log(`2 : ${JSON.stringify(response.data.Contents.map(object => object.Key))}`)
